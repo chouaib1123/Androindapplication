c52b6bde52f9add93a228907115cc39626df4178
chouaib <chouaibmonsif123@gmail.com>
2024-01-30 18:11:46 +0100

    5

--- app/src/main/java/com/example/myapplication/agencyregister.java
+++ app/src/main/java/com/example/myapplication/agencyregister.java
@@ -1,15 +1,71 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.TextView;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class agencyregister extends AppCompatActivity {
-
+    TextView selectedTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.agencyregister);
+        TextView cnss = findViewById(R.id.cnssreg);
+        setOnClickListenerForTextView(cnss);
+
+
+    }
+    private Map<Integer, TextView> clickedTextViewMap = new HashMap<>();
+
+
+    private void setOnClickListenerForTextView(final TextView textView) {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedTextView = textView;
+                startImagePickerForTextView();
+            }
+        });
     }
 
+    private void startImagePickerForTextView() {
+        ImagePicker.with(agencyregister.this)
+                .crop()                     // Crop image (Optional)
+                .compress(1024)             // Final image size will be less than 1 MB (Optional)
+                .maxResultSize(1080, 1080)  // Final image resolution will be less than 1080 x 1080 (Optional)
+                .start();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == ImagePicker.REQUEST_CODE && data != null) {
+            Uri uri = data.getData();
+            if (uri != null) {
+
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                    BitmapDrawable drawable = new BitmapDrawable(getResources(), bitmap);
+                    selectedTextView.setBackground(drawable);
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                }
+
+            }
+        }
+    }
 }
